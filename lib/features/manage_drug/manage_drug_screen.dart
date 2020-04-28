f52aa1a25f1f977b2f3a93feefbb2823b9ba3494
Andrey Gordeev <andrew8712@gmail.com>
2020-04-28 16:28:11 +0300

    Add ManageDrugScreen

---
+++ lib/features/manage_drug/manage_drug_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ManageDrugScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Drug'),
+      ),
+      body: Container(),
+    );
+  }
+}
