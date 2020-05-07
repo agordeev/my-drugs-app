eb591d59f66937f31c2cfb9356d09c22c12aeb04
Andrey Gordeev <andrew8712@gmail.com>
2020-05-07 10:46:22 +0300

    Disable landscape for Android phones

--- android/app/src/main/kotlin/com/example/my_drugs/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_drugs/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.andreygordeev.mydrugs
 
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(getResources().getBoolean(R.bool.portrait_only)){
+          setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
 }
